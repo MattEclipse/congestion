d6cfff91037b9f5532faa305bdf29ef54b78230b
MattEclipse <mmolenat@eclipse-flow.com>
2024-11-20 15:46:22 +0100

    supr havesine obsolet in utils

--- src/utils.py
+++ src/utils.py
@@ -121,23 +121,6 @@ def hist_plot_2(df, Y_name):
         
 # Specificaly for data/lignes-aeriennes-rte-nv.csv
 
-def haversine(lat1, lon1, lat2, lon2):
-    """
-    Calculate the great-circle distance between two points on the Earth's surface.
-    Inputs are in decimal degrees.
-    """
-    R = 6371e3  # Radius of Earth in meters
-    phi1 = math.radians(lat1)
-    phi2 = math.radians(lat2)
-    delta_phi = math.radians(lat2 - lat1)
-    delta_lambda = math.radians(lon2 - lon1)
-
-    a = math.sin(delta_phi / 2) ** 2 + math.cos(phi1) * math.cos(phi2) * math.sin(delta_lambda / 2) ** 2
-    c = 2 * math.atan2(math.sqrt(a), math.sqrt(1 - a))
-
-    return R * c  # Distance in meters
-
-
 def length_from_coordinate(longitudes, latitudes):
     
     total_length = 0
