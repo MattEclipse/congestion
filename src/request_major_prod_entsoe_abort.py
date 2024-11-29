e9fd38b6b4e1d6ad8bfff9fa24379a1ae6d403dd
MattEclipse <mmolenat@eclipse-flow.com>
2024-11-29 14:25:28 +0100

    rename : tempo abort, request prends trop de temsp

--- src/request_major_prod_entsoe_abort.py
+++ src/request_major_prod_entsoe_abort.py
@@ -7,13 +7,15 @@ from entsoe import EntsoePandasClient, EntsoeRawClient
 import pandas as pd
 from dateutil.relativedelta import relativedelta
 import time
+import numpy as np
+rng = np.random.standard_normal(2)  
 
 client_raw = EntsoeRawClient(api_key='4aa04087-0e00-4997-81c5-4f5c5c5881ef')
 client_pd = EntsoePandasClient(api_key= '4aa04087-0e00-4997-81c5-4f5c5c5881ef')
 
 start_str = '20230101'
 start = pd.Timestamp(start_str, tz='Europe/Brussels')
-end_str = '20230102' # 20241001
+end_str = '20230301' # 20241001
 end = pd.Timestamp(end_str, tz='Europe/Brussels')
 
 countries = ['FR'] 
@@ -23,13 +25,13 @@ for country_code in countries:
     current_date = start
 
     while current_date < end:
-        next_month = current_date + relativedelta(months=1)
+        next_date = current_date + relativedelta(days=2)
         
         # Query data for the current month
         df = client_pd.query_generation_per_plant(
             country_code=country_code,
             start=current_date,
-            end=next_month,
+            end=next_date,
             psr_type=None
         )
         
@@ -37,13 +39,13 @@ for country_code in countries:
         dataframes.append(df)
         
         # Move to the next month
-        current_date = next_month
+        current_date = next_date
 
     # Concatenate all DataFrames into one
     final_df = pd.concat(dataframes, ignore_index=False)
-    path_data_csv = "/Users/matthiasmolenat/repos/volatility/datas/gen_plant_from_" + start_str + '_to_' + end_str+ "_"+country_code+".csv"
+    path_data_csv = "/Users/matthiasmolenat/repos/congestion/data/gen_plant_from_" + start_str + '_to_' + end_str+ "_"+country_code+".csv"
     final_df.to_csv(path_data_csv)
     
-    time.sleep(10)
+    time.sleep(rng.random())
     
 
