b8d52d372b5b4ee80ae22c5359d4e7689ca4f953
MattEclipse <mmolenat@eclipse-flow.com>
2024-12-10 17:19:45 +0100

    RAS

--- notebooks/script_closest_neighboors.py
+++ notebooks/script_closest_neighboors.py
@@ -10,10 +10,11 @@ my_GeopointPoste_1 = [48.5587213, 7.7976421]
 my_GeopointPoste_2 = [49.3132494, 1.1897809]
 my_GeopointPoste_3 = [49.263092, 4.100068] # Cernay
 my_GeopointPoste_4 = [46.754655, 5.915800] # Champagnolle
-n_voisins = 10
+my_GeopointPoste_5 = [48.7098768941521, 1.3839132342552853] # Les Arpents
+n_voisins = 20
 
-df_res = get_closest_post(df,my_GeopointPoste_4, n_voisins)
+df_res = get_closest_post(df,my_GeopointPoste_5, n_voisins)
 
-df_res = df_res[["Code","Nom", "dist_to_my_poste_km","RAM_injection", "RAM_sous_tirage", "Capacite de transformation HTB/HTA restante disponible pour l'injection sur le reseau public de distribution"]]
+df_res = df_res[["Code","Nom", "dist_to_my_poste_km","RAM_injection", "RAM_sous_tirage", "Capacité de transformation HTB/HTA restante disponible pour l'injection sur le réseau public de distribution"]]
 
 df_res
